c2ad6bc609914679d4b921f880b4abb31b2fa737
Nikita Tsyruk <kievk81105@gmail.com>
2025-05-14 14:17:53 +0300

    update a test add parametrize tests use black to make project cleaner

--- test/__init__.py
+++ test/__init__.py
@@ -1,8 +1,8 @@
 """Initialization for unit test package of the SimpleBankSystem."""
 
-from service.user_service import *
-from service.account_service import *
-from service.file_manager import *
-from models.user import *
-from models.account import *
-from models.transaction import *
+from ..service.user_service import *
+from ..service.account_service import *
+from ..service.file_manager import *
+from ..models.user import *
+from ..models.account import *
+from ..models.transaction import *
