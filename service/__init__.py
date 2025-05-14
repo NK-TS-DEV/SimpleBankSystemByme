f5822880db56311ab7309f37d13b7ecff0048f92
Nikita Tsyruk <kievk81105@gmail.com>
2025-05-14 14:33:54 +0300

    update a test add parametrize tests use black to make project cleaner

--- service/__init__.py
+++ service/__init__.py
@@ -1,5 +1,5 @@
 """Initializes the service package by importing core models used in service classes."""
 
-from ..models.user import User
-from ..models.account import BankAccount
-from ..models.transaction import Transaction
+from models.user import User
+from models.account import BankAccount
+from models.transaction import Transaction
